72727b4e55335bff0f1797a7d00b45bdf778560a
Chimoha31 <mokochii1108@gmail.com>
2022-01-09 23:56:00 -0800

    style for background

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from "react";
+
+const api = {
+  key: "2b3f83a4da0b1d3916b750a912b291b2",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
   return (
-   <div>
-     Hello
-   </div>
+    <div className="app">
+      <main>
+        <div class="search-box">
+          <input 
+           type="text" 
+           className="search-bar" 
+           placeholder="Search..." />
+        </div>
+      </main>
+    </div>
   );
 }
 
